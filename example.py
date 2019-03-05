25bd5b4aa0c0ae916611f1e5c95654907838ae65
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2019-03-05 13:53:55 +0100

    Format code using black.

--- example.py
+++ example.py
@@ -1,33 +1,61 @@
 from math import pi
 from path_tracer import Camera, Material, Sphere, Triangle, PathTracer, Vec3, save_img
 
+
 def main():
-    camera = Camera(position=Vec3(0, 2, 8.5),
-                    target=Vec3(0, 0, 0),
-                    up=Vec3.up,
-                    resolution=(300, 300))
+    camera = Camera(
+        position=Vec3(0, 2, 8.5),
+        target=Vec3(0, 0, 0),
+        up=Vec3.up,
+        resolution=(300, 300),
+    )
     scene = (
-             Sphere(Vec3(-4.0, 0, 0), 2.0, Material(0, 1.0 / pi)),
-             Sphere(Vec3(0, -1.0, 3), 1.0, Material(0, 1.0 / pi)),
-             Sphere(Vec3(4.0, 1, 0.0), 3.0, Material(0, 1.0 / pi)),
-             Sphere(Vec3(0, 5, 0), 0.75, Material(19550, 0.0)),
-             Triangle(Vec3(-10,-2,-10), Vec3(10,-2,10), Vec3(10,-2,-10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,-10), Vec3(-10,-2,10), Vec3(10,-2,10), Material(0,1/pi)),
-             Triangle(Vec3(-10,6,-10), Vec3(10,6,-10), Vec3(10,6,10), Material(0,1/pi)),
-             Triangle(Vec3(-10,6,-10), Vec3(10,6,10), Vec3(-10,6,10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,-10), Vec3(10,-2,-10), Vec3(10,6,-10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,-10), Vec3(10,6,-10), Vec3(-10,6,-10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,-10), Vec3(-10,6,10), Vec3(-10,-2,10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,-10), Vec3(-10,6,-10), Vec3(-10,6,10), Material(0,1/pi)),
-             Triangle(Vec3(10,-2,-10), Vec3(10,-2,10), Vec3(10,6,10), Material(0,1/pi)),
-             Triangle(Vec3(10,-2,-10), Vec3(10,6,10), Vec3(10,6,-10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,10), Vec3(10,6,10), Vec3(10,-2,10), Material(0,1/pi)),
-             Triangle(Vec3(-10,-2,10), Vec3(-10,6,10), Vec3(10,6,10), Material(0,1/pi)),
+        Sphere(Vec3(-4.0, 0, 0), 2.0, Material(0, 1.0 / pi)),
+        Sphere(Vec3(0, -1.0, 3), 1.0, Material(0, 1.0 / pi)),
+        Sphere(Vec3(4.0, 1, 0.0), 3.0, Material(0, 1.0 / pi)),
+        Sphere(Vec3(0, 5, 0), 0.75, Material(19550, 0.0)),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(10, -2, 10), Vec3(10, -2, -10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(-10, -2, 10), Vec3(10, -2, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, 6, -10), Vec3(10, 6, -10), Vec3(10, 6, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, 6, -10), Vec3(10, 6, 10), Vec3(-10, 6, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(10, -2, -10), Vec3(10, 6, -10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(10, 6, -10), Vec3(-10, 6, -10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(-10, 6, 10), Vec3(-10, -2, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, -10), Vec3(-10, 6, -10), Vec3(-10, 6, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(10, -2, -10), Vec3(10, -2, 10), Vec3(10, 6, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(10, -2, -10), Vec3(10, 6, 10), Vec3(10, 6, -10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, 10), Vec3(10, 6, 10), Vec3(10, -2, 10), Material(0, 1 / pi)
+        ),
+        Triangle(
+            Vec3(-10, -2, 10), Vec3(-10, 6, 10), Vec3(10, 6, 10), Material(0, 1 / pi)
+        ),
     )
 
     ptracer = PathTracer()
     img = ptracer.render(camera, scene, 32)
     save_img("result.ppm", img)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- path_tracer.py
+++ path_tracer.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from datetime import datetime  
+from datetime import datetime
 import functools
 import math
 from multiprocessing import Pool
@@ -8,78 +8,85 @@ import random
 import time
 from typing import ClassVar, List, Optional, Tuple, Union
 
+
 @dataclass
 class Vec3:
-    x : float = 0.0
-    y : float = 0.0
-    z : float = 0.0
+    x: float = 0.0
+    y: float = 0.0
+    z: float = 0.0
 
-    up : ClassVar['Vec3']
-    down : ClassVar['Vec3']
-    one : ClassVar['Vec3']
-    zero : ClassVar['Vec3']
+    up: ClassVar["Vec3"]
+    down: ClassVar["Vec3"]
+    one: ClassVar["Vec3"]
+    zero: ClassVar["Vec3"]
 
-    def __add__(self, other) -> 'Vec3':
+    def __add__(self, other) -> "Vec3":
         return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self, other) -> 'Vec3':
+    def __sub__(self, other) -> "Vec3":
         return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
-    
-    def __mul__(self, value : float) -> 'Vec3':
+
+    def __mul__(self, value: float) -> "Vec3":
         return Vec3(self.x * value, self.y * value, self.z * value)
 
-    def __rmul__(self, value : float) -> 'Vec3':
+    def __rmul__(self, value: float) -> "Vec3":
         return self.__mul__(value)
 
-    def __neg__(self) -> 'Vec3':
+    def __neg__(self) -> "Vec3":
         return Vec3(-self.x, -self.y, -self.z)
 
-    def dot(self, other : 'Vec3') -> float:
+    def dot(self, other: "Vec3") -> float:
         return self.x * other.x + self.y * other.y + self.z * other.z
 
-    def cross(s, o : 'Vec3') -> 'Vec3':
-        return Vec3(s.y*o.z - s.z*o.y, s.z*o.x - s.x*o.z, s.x*o.y - s.y*o.x)
+    def cross(s, o: "Vec3") -> "Vec3":
+        return Vec3(s.y * o.z - s.z * o.y, s.z * o.x - s.x * o.z, s.x * o.y - s.y * o.x)
 
-    def normalize(self) -> 'Vec3':
+    def normalize(self) -> "Vec3":
         length = math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
         self.x /= length
         self.y /= length
         self.z /= length
         return self
 
+
 Vec3.up = Vec3(0.0, 1.0, 0.0)
 Vec3.down = Vec3(0.0, -1.0, 0.0)
 Vec3.zero = Vec3(0.0, 0.0, 0.0)
 Vec3.one = Vec3(1.0, 1.0, 1.0)
 
+
 @dataclass
 class Material:
-    emittance : float = 0.0
-    reflectance : float = 0.0
+    emittance: float = 0.0
+    reflectance: float = 0.0
+
 
 @dataclass
 class Sphere:
-    center : Vec3
-    radius : float
-    material : Material
+    center: Vec3
+    radius: float
+    material: Material
+
 
 @dataclass
 class Triangle:
-    a : Vec3
-    b : Vec3
-    c : Vec3
-    material : Material = Material()
+    a: Vec3
+    b: Vec3
+    c: Vec3
+    material: Material = Material()
 
     def __post_init__(self):
         self.normal = (self.a - self.b).cross(self.a - self.c).normalize()
 
+
 @dataclass
 class Ray:
-    origin : Vec3
-    dir : Vec3
+    origin: Vec3
+    dir: Vec3
 
-    IntersectionResult = namedtuple("IntersectionResult",
-                                    "success distance intersection normal item")
+    IntersectionResult = namedtuple(
+        "IntersectionResult", "success distance intersection normal item"
+    )
 
     def intersect(self, obj) -> IntersectionResult:
         if isinstance(obj, Sphere):
@@ -87,24 +94,20 @@ class Ray:
         elif isinstance(obj, Triangle):
             return self._intersect_triangle(obj)
         else:
-            assert(False, "Unsupported intersection type")
-            return Ray.IntersectionResult(success=False,
-                                          distance=0,
-                                          intersection=None,
-                                          normal=None,
-                                          item=None)
+            assert (False, "Unsupported intersection type")
+            return Ray.IntersectionResult(
+                success=False, distance=0, intersection=None, normal=None, item=None
+            )
 
-    def _intersect_sphere(self, sphere : Sphere) -> IntersectionResult:
+    def _intersect_sphere(self, sphere: Sphere) -> IntersectionResult:
         oc = self.origin - sphere.center
         a = self.dir.dot(self.dir)
         b = 2.0 * oc.dot(self.dir)
         c = oc.dot(oc) - sphere.radius * sphere.radius
-        discriminant = b*b - 4*a*c
-        no_intersect = Ray.IntersectionResult(success=False,
-                                              distance=0,
-                                              intersection=None,
-                                              normal=None,
-                                              item=None)
+        discriminant = b * b - 4 * a * c
+        no_intersect = Ray.IntersectionResult(
+            success=False, distance=0, intersection=None, normal=None, item=None
+        )
         if discriminant < 0.0:
             return no_intersect
         else:
@@ -112,22 +115,22 @@ class Ray:
             if distance < 0.0:
                 return no_intersect
             intersection = self.origin + distance * self.dir
-            normal = (intersection - sphere.center).normalize() 
-            return Ray.IntersectionResult(success=True,
-                                          distance=distance,
-                                          intersection=intersection,
-                                          normal=normal,
-                                          item=sphere)
+            normal = (intersection - sphere.center).normalize()
+            return Ray.IntersectionResult(
+                success=True,
+                distance=distance,
+                intersection=intersection,
+                normal=normal,
+                item=sphere,
+            )
 
-    def _intersect_triangle(self, triangle : Triangle) -> IntersectionResult:
+    def _intersect_triangle(self, triangle: Triangle) -> IntersectionResult:
         """ Möller–Trumbore intersection """
 
-        no_intersect = Ray.IntersectionResult(success=False,
-                                              distance=0,
-                                              intersection=None,
-                                              normal=None,
-                                              item=None)
-        epsilon = 0.000001
+        no_intersect = Ray.IntersectionResult(
+            success=False, distance=0, intersection=None, normal=None, item=None
+        )
+        epsilon = 0.000_001
         v0, v1, v2 = triangle.a, triangle.b, triangle.c
         edge1 = v1 - v0
         edge2 = v2 - v0
@@ -147,36 +150,39 @@ class Ray:
         distance = f * edge2.dot(q)
         if distance > epsilon:
             intersection = self.origin + self.dir * distance
-            return Ray.IntersectionResult(success=True,
-                                          distance=distance,
-                                          intersection=intersection,
-                                          normal=triangle.normal,
-                                          item=triangle)
+            return Ray.IntersectionResult(
+                success=True,
+                distance=distance,
+                intersection=intersection,
+                normal=triangle.normal,
+                item=triangle,
+            )
         else:
             return no_intersect
 
+
 @dataclass
 class Camera:
-    position : Vec3
-    target : Vec3
-    up : Vec3
+    position: Vec3
+    target: Vec3
+    up: Vec3
 
-    resolution : tuple = (100, 100)
+    resolution: tuple = (100, 100)
 
     def __post_init__(self):
         self.dir = (self.target - self.position).normalize()
         self.left = self.dir.cross(self.up)
         self.up = self.left.cross(self.dir)
 
-
-    def generate_ray(self, i : int, j : int) -> Ray:
+    def generate_ray(self, i: int, j: int) -> Ray:
         ray_origin = self.position
         scale_left = -1.0 + 2.0 * i / self.resolution[0]
         scale_up = -1.0 + 2.0 * j / self.resolution[1]
         ray_dir = self.dir + self.left * scale_left + self.up * scale_up
         return Ray(ray_origin, ray_dir.normalize())
 
-def save_img(filepath : str, img : List[List[int]]):
+
+def save_img(filepath: str, img: List[List[int]]):
     with open(filepath, "w") as f:
         f.write("P2\n")
         f.write(f"{len(img[0])} {len(img)}\n")
@@ -185,8 +191,9 @@ def save_img(filepath : str, img : List[List[int]]):
             for color in row:
                 f.write(f"{color} ")
             f.write("\n")
-            
-def random_unit_vec_in_hemisphere(normal : Vec3) -> Vec3:
+
+
+def random_unit_vec_in_hemisphere(normal: Vec3) -> Vec3:
     theta = math.pi * random.uniform(0, 1)
     phi = 2 * math.pi * random.uniform(0, 1)
     sin_theta = math.sin(theta)
@@ -199,12 +206,15 @@ def random_unit_vec_in_hemisphere(normal : Vec3) -> Vec3:
     else:
         return -result
 
+
 class PathTracer:
-    def render(self,
-              camera : Camera,
-              scene : Tuple[Triangle, Sphere],
-              samples: int) -> List[List[int]]:
-        img = [[0 for x in range(camera.resolution[0])] for x in range(camera.resolution[1])]
+    def render(
+        self, camera: Camera, scene: Tuple[Triangle, Sphere], samples: int
+    ) -> List[List[int]]:
+        img = [
+            [0 for x in range(camera.resolution[0])]
+            for x in range(camera.resolution[1])
+        ]
 
         def args_provider():
             nonlocal samples, camera, scene
@@ -219,9 +229,11 @@ class PathTracer:
 
         process_count = 4
         with Pool(processes=process_count, initializer=init_process) as pool:
-            colors = pool.imap(self._path_trace_from_tuple,
-                               args_provider(),
-                               samples * camera.resolution[0])
+            colors = pool.imap(
+                self._path_trace_from_tuple,
+                args_provider(),
+                samples * camera.resolution[0],
+            )
             for j in range(camera.resolution[1]):
                 for i in range(camera.resolution[0]):
                     for s in range(samples):
@@ -233,20 +245,24 @@ class PathTracer:
 
         return img
 
-    def path_trace(self,
-                   ray : Ray,
-                   depth : int,
-                   items : Tuple[Triangle, Sphere],
-                   from_item : Optional[Union[Triangle, Sphere]] = None) -> int:
+    def path_trace(
+        self,
+        ray: Ray,
+        depth: int,
+        items: Tuple[Triangle, Sphere],
+        from_item: Optional[Union[Triangle, Sphere]] = None,
+    ) -> int:
         max_depth = 5
         if depth >= max_depth:
             return 0
 
-        closest_hit = Ray.IntersectionResult(success=False,
-                                             distance=999999999.9,
-                                             intersection=None,
-                                             normal=None,
-                                             item=None)
+        closest_hit = Ray.IntersectionResult(
+            success=False,
+            distance=999_999_999.9,
+            intersection=None,
+            normal=None,
+            item=None,
+        )
         for item in items:
             if (from_item is not None) and (item is from_item):
                 continue
@@ -269,8 +285,10 @@ class PathTracer:
         final_color = emittance + (reflectance * color_incoming * cos_theta * 2.0)
         return final_color
 
-    def _path_trace_from_tuple(self, args : Tuple[Ray,
-                                            int,
-                                            Tuple[Triangle, Sphere],
-                                            Optional[Union[Triangle, Sphere]]]) -> int:
+    def _path_trace_from_tuple(
+        self,
+        args: Tuple[
+            Ray, int, Tuple[Triangle, Sphere], Optional[Union[Triangle, Sphere]]
+        ],
+    ) -> int:
         return self.path_trace(*args)
